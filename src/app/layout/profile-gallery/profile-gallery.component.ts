7efd2b1e4e7f131af7bf7a77d7fcf322bab86eca
Free Hoebeke <freehoebeke@gmail.com>
2021-11-29 14:13:15 +0100

    codelab03

---
+++ src/app/layout/profile-gallery/profile-gallery.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {PetService} from "../../service/pet.service";
+import {Pet} from "../../model/Pet";
+
+@Component({
+  selector: 'app-profile-gallery',
+  templateUrl: './profile-gallery.component.html',
+  styleUrls: ['./profile-gallery.component.css']
+})
+export class ProfileGalleryComponent implements OnInit {
+  pets : Pet[];
+
+  constructor(private petService : PetService) {
+    this.pets = [];
+  }
+
+  ngOnInit(): void {
+    this.getPets();
+  }
+
+  getPets(): void {
+      this.petService.getPets().subscribe(output => this.pets = output);
+  }
+
+}
